8644ff06920f76d7de54a3fb05847910a8ac31a3
Deepali Kadway <dkadway@gmail.com>
2025-07-30 19:15:22 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ const express = require('express'); // Importing the express module
 
 const app = express()
 
+// localhost:3000/
 app.get('/', (req, res) => {
   res.send('Hello World')
+})
+
+// localhost:3000/hello2
+app.get('/hello2', (req, res) => {
+  res.send("Hello World 2.0")
 })
 
 app.listen(3000)
